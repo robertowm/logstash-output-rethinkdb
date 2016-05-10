42fe8843d999577e7c5e248b7776d62a86744d23
Roberto Menezes <rmenezes@stone.com.br>
2016-05-10 15:56:07 -0300

    Adjust insertion at RethinkDB
    Now it inserts the documents correctly.  We had to change the code
    and implement a simple mapping between an logstash event to a
    ruby hash.  Also, we detect date formats and parse it to a Time
    object.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 gemspec
-gem "logstash", :github => "elastic/logstash", :branch => "2.3"
-gem "rethinkdb", ">= 2.3.0.0", "< 2.4.0"
+gem "rethinkdb"
--- lib/logstash/outputs/rethinkdb.rb
+++ lib/logstash/outputs/rethinkdb.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 require "logstash/outputs/base"
 require "logstash/namespace"
+require "logstash/timestamp"
+require 'time'
 require "rethinkdb"
 include RethinkDB::Shortcuts
 
 # An example output that does nothing.
 class LogStash::Outputs::RethinkDB < LogStash::Outputs::Base
   config_name "rethinkdb"
-
-  default :codec, "json"
 
   config :host,  :validate => :string, :default => "localhost"
   config :port,  :validate => :number, :default => 28015
@@ -33,23 +33,40 @@ class LogStash::Outputs::RethinkDB < LogStash::Outputs::Base
       logger.info "Database `#{@db}` already exists."
     end
 
-    @codec.on_event(&method(:send_to_rethinkdb))
+    begin
+      r.db(@db).table_create(@table).run(@conn)
+    rescue RethinkDB::RqlRuntimeError => err
+      logger.info "Table `#{@db}.#{@table}` already exists."
+    end
   end # def register
 
   public
   def receive(event)
-    begin
-      @codec.encode(event)
-    rescue LocalJumpError
-      raise
-    rescue StandardError => e
-      @logger.warn("Error encoding event", :exception => e, :event => event)
-    end
+    obj = format_event(event)
+    r.db(@db)
+      .table(@table)
+      .insert(obj)
+      .run(@conn)
   end # def event
 
-  def send_to_rethinkdb(event, payload)
-    r.table(@table)
-      .insert(payload)
-      .run(@conn)
+  def format_event(event)
+    Hash[event.to_hash.map{ |k, v| [k, format_value(v)] }]
   end
+
+  def format_value(value)
+    case value
+    when LogStash::Timestamp
+      Time.parse(value.to_iso8601)
+    when BigDecimal
+      value.to_f()
+    when String
+      if value !~ /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
+        value
+      else
+        Time.parse(value)
+      end
+    else
+      value
+    end
+  end # def formatValue
 end # class LogStash::Outputs::RethinkDB
